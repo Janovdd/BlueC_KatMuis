0895317f4cc1488529994dfe1634a1460467aa38
Andries <nieu0082@hz.nl>
2017-11-13 13:23:47 +0100

    Muis toegevoegd.

---
+++ ConsoleApp1/ConsoleApp1/Muis.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Muis
+    {
+        private bool isLevend;
+
+        public Muis()
+        {
+            // zet isLevend op true
+        }
+
+        public void Kill()
+        {
+            // zet isLevend op false
+        }
+
+        public override string ToString()
+        {
+            if(this.isLevend)
+            {
+                return "Nog steeds levend haha!";
+            } else
+            {
+                return "...";
+            }
+        }
+    }
+}
